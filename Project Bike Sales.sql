ec5a92f07f314aae3da8ca013516c36083306e67
Fuad Abdurahman <140121448+FuadAbdurahman@users.noreply.github.com>
2023-07-22 06:16:50 +0700

    Add files via upload

---
+++ Project Bike Sales.sql
@@ -0,0 +1,44 @@
+SELECT
+
+	ord.order_id,
+	CONCAT(cus.first_name,' ',cus.last_name) AS 'customer', -- concat adalah penggabungan kolom
+	cus.city,
+	cus.state,
+	ord.order_date,
+	SUM(ite.quantity) AS 'total units',
+	SUM(ite.quantity * ite.list_price) 'revenue',
+	pro.product_name,
+	cat.category_name,
+	bra.brand_name,
+	sto.store_name,
+	CONCAT(sta.first_name,' ',sta.last_name) AS 'Sales_Rep' 
+	
+
+FROM
+		sales.orders ord -- prefix ord
+JOIN	sales.customers cus -- prefix cus
+ON		ord.customer_id = cus.customer_id -- disambungkan melalui customer_id
+JOIN	sales.order_items ite
+ON		ord.order_id = ite.order_id
+JOIN	production.products pro
+ON		ite.product_id = pro.product_id
+JOIN	production.categories cat
+ON		pro.category_id = cat.category_id
+JOIN	production.brands bra
+ON		bra.brand_id = pro.brand_id
+JOIN	sales.stores sto
+ON		sto.store_id = ord.store_id
+JOIN	sales.staffs sta
+ON		sta.staff_id = ord.staff_id
+
+GROUP BY
+	ord.order_id,
+	CONCAT(cus.first_name,' ',cus.last_name),
+	cus.city,
+	cus.state,
+	ord.order_date,
+	pro.product_name,
+	cat.category_name,
+	bra.brand_name,
+	sto.store_name,
+	CONCAT(sta.first_name,' ',sta.last_name)
